71f24a4141d9133db00c1269f27493131b115318
Vitor Aspirot <vitoraspirot@ivitoraspirot.local>
2022-07-30 15:15:31 -0300

    feat: initial commit

---
+++ github-repositories/github-repositories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  github-repositories
+//
+//  Created by Vitor Aspirot on 30/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
