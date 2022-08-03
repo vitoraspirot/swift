5215e473da51e909ad201b817d0da623bbc7e1c6
Vitor Aspirot <vitoraspirot@ivitoraspirot.local>
2022-08-03 15:34:01 -0300

    feat: add Podedex project

--- github-repositories/Pods/Target Support Files/Pods-github-repositories-github-repositoriesTests/Pods-github-repositories-github-repositoriesTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_github_repositories_github_repositoriesTests : NSObject
-@end
-@implementation PodsDummy_Pods_github_repositories_github_repositoriesTests
-@end
